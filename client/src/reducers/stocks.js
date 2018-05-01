44cc16fe31c021608f709f6b87141f62bbb10c63
thomlomDEV <38853595+thomlomDEV@users.noreply.github.com>
2018-05-01 09:23:14 +0200

    Create stocks.js

---
+++ client/src/reducers/stocks.js
@@ -0,0 +1,23 @@
+import { ADD_STOCK, REMOVE_STOCK, LOAD_STOCKS } from '../actions/stock';
+
+const stocks = (state = [], action) => {
+  switch (action.type) {
+    case ADD_STOCK:
+      return [
+        ...state, {
+          code: action.code,
+          data: action.data,
+          description: action.description,
+          id: action.id
+        }
+      ]
+    case LOAD_STOCKS:
+      return state.concat(action.stocks);
+    case REMOVE_STOCK:
+      return state.filter(stock => stock.id !== action.id);
+    default:
+      return state;
+  }
+};
+
+export default stocks;
